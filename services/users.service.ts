54155885c8ab37a5b26b7f1452145f74d6e62b42
aycast <ayca-temel@hotmail.com>
2024-05-05 19:46:40 +0300

    added validations and validation error type

--- services/users.service.ts
+++ services/users.service.ts
@@ -1,6 +1,7 @@
 import { PrismaClient, User } from "@prisma/client";
 import { Service } from "./service";
 import { hashPassword } from "../utils/secret"
+import { UserValidator } from "../validations/user.validator";
 
 export class UserService extends Service {
 
@@ -10,12 +11,15 @@ export class UserService extends Service {
 
     public async create(user : User): Promise<User> {
 
+        const validator = new UserValidator(user);
+        validator.validate();
+
         user.password = await hashPassword(user.password);
 
         return await this.repository.user.create({
             data : user
         });
-        
+
     }
 
 }
---
+++ validations/user.validator.ts
@@ -0,0 +1,54 @@
+import { User } from "@prisma/client";
+import { ValidationError } from "../errors/validation.error";
+import { Validator } from "./validator";
+
+const rules = { 
+  email : {
+    name : "email",
+    rule : /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    message : "Lütfen geçerli bir e-posta giriniz."
+  },
+  username : {
+    name : "username",
+    rule : /^.{5,16}$/,
+    message : "Kullanıcı adı 5 ile 16 karakter arasında olmalıdır."
+  },
+  password : {
+    name : "password",
+    rule : /^.{10,}$/,
+    message : "Şifre en az 10 karakter olmalıdır"
+  },
+}
+
+type fieldType = "email" | "username" | "password";
+
+export class UserValidator extends Validator {
+
+  user : User;
+  fields : fieldType[];
+
+  constructor(user : User, fields : fieldType[] = ["email", "username", "password"]) {
+    super();
+    this.user = user;
+    this.fields = fields
+  }
+
+  validate() {
+    
+    const failedFields : {name: string, message: string}[] = [];
+    
+    for (let field of this.fields) {
+      if (!this.validateField(this.user[field], rules[field].rule)) {
+        failedFields.push({
+          name: rules[field].name,
+          message : rules[field].message 
+        })
+      }
+    }
+
+    if (failedFields.length != 0) throw new ValidationError("user validation error", failedFields);
+  
+  }
+
+
+}
