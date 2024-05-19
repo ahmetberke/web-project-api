c7736b43f4c57f14c1475a4655b65f3d4a69ab67
ahmetberke <tuncel.ahmet.berke@gmail.com>
2024-05-19 13:48:39 +0300

    added api infstructure

---
+++ api/middlewares/error.middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "../../errors/error";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { UniqueError } from "../../errors/unique.error";
+import { INTERNAL_SERVER_ERROR } from "../../errors/server.error";
+
+export class ErrorMiddleware {
+  static handle = async (err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    switch(true) {
+      case err instanceof HttpError:
+        res.status(err.code).json(err.response());
+      case err instanceof PrismaClientKnownRequestError:
+        const name = ((err as PrismaClientKnownRequestError).meta!.target as string).split("_")[1];
+        const prismaValidationError = new UniqueError(`${name} zaten kullanılıyor`,{
+          name
+        });
+        res.status(prismaValidationError.code).json(prismaValidationError.response());
+        break;
+      default:
+        res.status(INTERNAL_SERVER_ERROR.code).json(INTERNAL_SERVER_ERROR.response());
+    }
+
+  }
+}
